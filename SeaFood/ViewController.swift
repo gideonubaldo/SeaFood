85c3593f8c4742eccfd868ae7680e87311de1591
Gideon <gideon.ubaldo@sjsu.edu>
2019-01-17 12:47:48 -0800

    Initial Commit

---
+++ SeaFood/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  SeaFood
+//
+//  Created by Gideon Ubaldo on 1/17/19.
+//  Copyright © 2019 Gideon Ubaldo. All rights reserved.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
